ca0d2e9d5afd2cfbc846fd4fc7d33c8670e4fcbc
brandonjohnson21 <brandon.johnson.21@us.af.mil>
2021-08-28 12:50:40 -0700

    add more ntr clock games

--- SD Card/sd/arm9/include/twlClockExcludeMap.h
+++ SD Card/sd/arm9/include/twlClockExcludeMap.h
@@ -2,6 +2,10 @@
 #define TWLCLOCKEXCLUDEMAP_H
 
 static const char twlClockExcludeList[][4] = {
+	"CRL", // Coraline
+	"CLP", // Club Pengion: Elite Penguin Force
+	"CY9", // Club Penguin: EPF: Herbert's Revenge
+	"ATD", // Clubhouse Games
 	"YGX", // Grand Theft Auto: Chinatown Wars
 	"C6C", // Infinite Space
 	"B6F", // LEGO Batman 2: DC Super Heroes
@@ -20,6 +24,8 @@ static const char twlClockExcludeList[][4] = {
 	"YLG", // LEGO Star Wars: The Complete Saga
 	"BL9", // LEGO Star Wars III: The Clone Wars
 	"AY9", // Mario & Sonic at the Olympic Games
+	"BZP", // Peppa Pig: Theme Park Fun
+	"AQW", // Puzzle Quest: Challenge of the Warlords
 	"ASC", // Sonic Rush
 	"ASM", // Super Mario 64 DS
 };
